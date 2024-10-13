fcda412a30713e9182569f1cb4af75d192fc78bf
Xavier Leclercq <xavier@leclercq.dev>
2024-10-13 22:32:40 +0100

    Refactoring (#2)

---
+++ tests/src/HTMLParserTests/HTMLParserTests.cpp
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2005-2024 Xavier Leclercq
+// SPDX-License-Identifier: BSL-1.0
+
+#include "HTMLParserTests.h"
+
+using namespace Ishiko;
+
+HTMLParserTests::HTMLParserTests(const TestNumber& number, const TestContext& context)
+    : TestSequence(number, "HTMLParser tests", context)
+{
+}
---
+++ tests/src/HTMLParserTests/HTMLParserTests.h
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2005-2024 Xavier Leclercq
+// SPDX-License-Identifier: BSL-1.0
+
+#ifndef GUARD_ISHIKO_CPP_HTML_TESTS_HTMLPARSERTESTS_H
+#define GUARD_ISHIKO_CPP_HTML_TESTS_HTMLPARSERTESTS_H
+
+#include <Ishiko/TestFramework/Core.hpp>
+
+class HTMLParserTests : public Ishiko::TestSequence
+{
+public:
+    HTMLParserTests(const Ishiko::TestNumber& number, const Ishiko::TestContext& context);
+};
+
+#endif
