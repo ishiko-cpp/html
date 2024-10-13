959e01985d40afa7648a7f31a1c82f80bf78468b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-10-13 23:28:13 +0100

    Updates

--- tests/src/HTMLParserTests/HTMLParserTests.cpp
+++ tests/src/HTMLParserTests/HTMLParserTests.cpp
@@ -2,10 +2,19 @@
 // SPDX-License-Identifier: BSL-1.0
 
 #include "HTMLParserTests.h"
+#include "Ishiko/HTML/HTMLParser.h"
 
 using namespace Ishiko;
 
 HTMLParserTests::HTMLParserTests(const TestNumber& number, const TestContext& context)
     : TestSequence(number, "HTMLParser tests", context)
 {
+    append<HeapAllocationErrorsTest>("Constructor test 1", ConstructorTest1);
+}
+
+void HTMLParserTests::ConstructorTest1(Test & test)
+{
+    HTMLParser html_parser;
+
+    ISHIKO_TEST_PASS();
 }
